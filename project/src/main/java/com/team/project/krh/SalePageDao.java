21813f7f4e32c1d2dbc5e188ddd47f524cba88ca
ryumm <smileryunhah@naver.com>
2024-07-04 18:52:11 +0900

    김륜하 0704 18:52

--- project/src/main/java/com/team/project/krh/SalePageDao.java
+++ project/src/main/java/com/team/project/krh/SalePageDao.java
@@ -14,18 +14,41 @@ public class SalePageDao {
         session=new MyFactory().getSession();
     }
 
-    public List<ProductVo> salepagesearch(String findStr){
-        List<ProductVo> list =null;
-        session=new MyFactory().getSession();
-        list=session.selectList("salestock.salepagesearch",findStr);
+    public Map<String,Object> salepagesearch(Page page){
+        Map<String, Object> map = new HashMap<>();
+        List<ProductVo> list=null;
+        session = new MyFactory().getSession();
+        int totSize = session.selectOne("salestock.totSize", page.getFindStr());
+        page.setTotSize(totSize);
+        page.compute();
+        System.out.println(page);
+        list = session.selectList("salestock.salepagesearch", page);
+        map.put("page", page);
+        map.put("list", list);
         session.close();
-        return list;
+        return map;
     }
 
     public List<ProductVo> salepage_cheap(ProductVo vo){
         List<ProductVo> list=null;
         session=new MyFactory().getSession();
         list=session.selectList("salestock.salepage_cheap",vo);
+        session.close();
+        return list;
+    }
+
+    public List<ProductVo> salepage_expensive(ProductVo vo){
+        List<ProductVo> list = null;
+        session=new MyFactory().getSession();
+        list=session.selectList("salestock.salepage_expensive", vo);
+        session.close();
+        return list;
+    }
+
+    public List<ProductVo> salepage_new(ProductVo vo){
+        List<ProductVo> list=null;
+        session=new MyFactory().getSession();
+        list=session.selectList("salestock.salepage_new",vo);
         session.close();
         return list;
     }
