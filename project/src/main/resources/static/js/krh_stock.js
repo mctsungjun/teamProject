a9c0a00688cd7498d33184d3ac91dae9a5c0aab4
ryumm <smileryunhah@naver.com>
2024-07-09 13:27:52 +0900

    13:23

--- project/src/main/resources/static/js/krh_stock.js
+++ project/src/main/resources/static/js/krh_stock.js
@@ -68,24 +68,24 @@ function stockloadItem(findStr,nowPage){
     })
 }
 
-$.ajax({
-    url: "/stockgraph",
-    type: "GET",
-    data: {},
-    dataType: "json",
-    success: function(response) {
-        if (response.x.length === 0 && response.y.length === 0) {
-            console.log("데이터가 없습니다.");
-            // 또는 사용자에게 알리는 등의 처리를 추가할 수 있습니다.
-        } else {
-            // 데이터가 있는 경우 처리
-            console.log(response);
-        }
-    },
-    error: function() {
-        alert("데이터를 가져오는데 실패했습니다.");
-    }
-});
+// $.ajax({
+//     url: "/stockgraph",
+//     type: "GET",
+//     data: {},
+//     dataType: "json",
+//     success: function(response) {
+//         if (response.x.length === 0 && response.y.length === 0) {
+//             console.log("데이터가 없습니다.");
+//             // 또는 사용자에게 알리는 등의 처리를 추가할 수 있습니다.
+//         } else {
+//             // 데이터가 있는 경우 처리
+//             console.log(response);
+//         }
+//     },
+//     error: function() {
+//         alert("데이터를 가져오는데 실패했습니다.");
+//     }
+// });
 
 function graph(){
     $.ajax({
